9ce1c60a2cb00ba513328760647f8ab34eb63417
Muddathir Julie <107460369+Muddaj7@users.noreply.github.com>
2024-04-03 14:44:58 +0200

    updated menu

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var hamburger = document.getElementById("hamburger");
-    var navList = document.getElementById("nav-list");
+const hamburgerButton = document.getElementById('hamburger');
+const navList = document.getElementById('nav-list');
 
-    hamburger.addEventListener("click", function () {
-      navList.classList.toggle("show");
+function toggleButton() {
+  navList.classList.toggle('show');
+}
+
+function closeMenu() {
+  navList.classList.remove('show');
+}
+
+hamburgerButton.addEventListener('click', toggleButton);
+
+document.addEventListener('touchstart', function(event) {
+  if (!event.target.closest('#nav-list') && !event.target.closest('#hamburger')) {
+    closeMenu();
+  }
+});
+
+document.addEventListener('scroll', closeMenu);
+
+document.querySelectorAll('#nav-list a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetSection = document.querySelector(this.getAttribute('href'));
+
+    window.scrollTo({
+      top: targetSection.offsetTop,
+      behavior: 'smooth'
     });
+    closeMenu();
   });
+});
